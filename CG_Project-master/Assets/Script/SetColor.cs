2d44842abe5dab7a894186f3c8ffe9f91662565b
log-m <loganmorris15@yahoo.com>
2017-11-26 13:08:43 -0600

    new

---
+++ CG_Project-master/Assets/Script/SetColor.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace Script
+{
+	public class SetColor : MonoBehaviour
+	{
+
+		public Texture2D Blank;
+        //public GameObject template;
+		private Renderer _childRender;
+		private Texture2D _newBlack;
+		private float _timeLeft;
+		private float _numObjects;
+        private MaterialPropertyBlock prop;
+        Color32[] pixels;
+
+        // Use this for initialization
+        private void Start()
+		{
+            prop = new MaterialPropertyBlock();
+			_timeLeft = 0.0f;
+			_numObjects = 0.0f;
+			print("percentage done: " + _timeLeft);
+			RecursiveCount(transform);
+			_timeLeft += 1.0f / _numObjects;
+            _newBlack = new Texture2D(Blank.width, Blank.height);
+            pixels = new Color32[Blank.width * Blank.height];
+            Color32 b = new Color32(0, 0, 0, 255);
+            for(int i = 0; i < pixels.Length;i++){
+                pixels[i] = b;
+            }
+            _newBlack.SetPixels32(pixels);
+            _newBlack.Apply();
+            _childRender = transform.GetComponent<Renderer>();
+            _newBlack = new Texture2D(Blank.width, Blank.height);
+            _newBlack.SetPixels32(pixels);
+            _newBlack.Apply();
+            _childRender.GetPropertyBlock(prop);
+            prop.SetTexture("_MainTex", _newBlack);
+            _childRender.SetPropertyBlock(prop);
+            StartCoroutine(RecursiveColor(transform));
+           
+
+        }
+
+        private void RecursiveCount(Transform t)
+		{
+			if (t.childCount == 0)
+			{
+				_numObjects += 1.0f;
+				
+			}
+			else
+			{
+				for (var i = 0; i < t.childCount; i++)
+				{
+					RecursiveCount(t.GetChild(i));
+				}
+			}
+		}
+
+		private IEnumerator RecursiveColor(Transform t)
+		{
+			//base case
+			if (t.childCount == 0)
+			{
+                
+                _childRender = t.GetComponent<Renderer>();
+               _newBlack = new Texture2D(Blank.width, Blank.height);
+                _newBlack.SetPixels32(pixels);
+                _newBlack.Apply();
+                _childRender.GetPropertyBlock(prop);
+                prop.SetTexture("_MainTex", _newBlack);
+                _childRender.SetPropertyBlock(prop);
+                print("percentage done: " + _timeLeft);
+                //foreach (var VARIABLE in _childRender.materials)
+                //{
+                //_newBlack = new Texture2D(Blank.width, Blank.height);
+
+                /*for (var i = 0; i < Blank.width; i++)
+                {
+                    for (var j = 0; j < Blank.height; j++)
+                    {
+                        _newBlack.SetPixel(i, j, Color.black);
+                    }
+                }*/
+
+                /*_newBlack.Apply();
+                VARIABLE.mainTexture = _newBlack;*/
+                /*for(int i = 0; i < _childRender.materials.Length; i++)
+                {
+                    someobject.renderer.material = new Material(Shader.Find("Diffuse"));
+                    someobject.renderer.material.color = Color.blue;
+                    someobject.renderer.material.mainTexture = sometexture2D
+                    Material newMaterial = Instantiate(template.GetComponent<Renderer>().material);
+                    _childRender.materials[i] = new Material(Shader.Find("Unlit/Distance"));
+                    _childRender.materials[i].mainTexture = _newBlack;
+                    
+				}*/
+
+                _timeLeft += 1.0f / _numObjects;
+				
+				yield return 0;
+			}
+			else
+			{
+				for (var i = 0; i < t.childCount; i++)
+				{
+					//yield return StartCoroutine(RecursiveColor(t.GetChild(i)));
+					yield return RecursiveColor(t.GetChild(i));
+				}
+			}
+		}
+	}
+}
---
+++ CG_Project-master/Assets/Script/Shoot.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Script
+{
+	public class Shoot : MonoBehaviour 
+	{
+		public Camera Cam;
+		public int Radius;
+		public int ConeRadius;
+        private MaterialPropertyBlock prop;
+		private void Start()
+		{
+            prop = new MaterialPropertyBlock();
+			Cam = transform.GetComponent<Camera>();
+		}
+
+		private void Update()
+		{
+			if (!Input.GetMouseButton(0))
+				return;
+
+			RaycastHit hit;
+
+			var angle = Random.Range(0.0f, 1.0f) * Mathf.PI * 2.0f;
+			var radius = Mathf.Sqrt(Random.Range(0.0f, 1.0f)) * ConeRadius;
+			var newCenterX = Input.mousePosition.x + radius * Mathf.Cos(angle);
+			var newCenterY = Input.mousePosition.y + radius * Mathf.Sin(angle);
+			
+			if (!Physics.Raycast(Cam.ScreenPointToRay(new Vector2(newCenterX, newCenterY)), out hit))
+				return;
+			
+			var rend = hit.transform.GetComponent<Renderer>();
+			var meshCollider = hit.collider as MeshCollider;
+
+			if (rend == null || rend.sharedMaterial == null ||  meshCollider == null)
+				return;
+            rend.GetPropertyBlock(prop);
+			var tex = prop.GetTexture("_MainTex") as Texture2D;
+            
+            var pixelUv = hit.textureCoord;
+			if (tex == null) return;
+			
+			pixelUv.x *= tex.width;
+			pixelUv.y *= tex.height;
+           
+            StartCoroutine(DrawCircle(tex,rend, new Vector2(pixelUv.x, pixelUv.y), hit.distance));
+		}
+
+		private IEnumerator DrawCircle(Texture2D tex, Renderer rend, Vector2 center, float distance)
+		{
+			int x;
+            /* var tex2 = new Texture2D(tex.width, tex.height);
+            for (var i = 0; i < tex.width; i++)
+            {
+                for (var j = 0; j < tex.height; j++)
+                {
+                    tex2.SetPixel(i, j, tex.GetPixel(i, j));
+                }
+            }*/
+			for (x = 0; x <= Radius; x++)
+			{
+				var d = (int)Mathf.Ceil(Mathf.Sqrt(Radius * Radius - x * x));
+				int y;
+				for (y = 0; y <= d; y++)
+				{
+					var px = Mathf.FloorToInt(center.x + x + 0.5f);
+					var nx = Mathf.FloorToInt(center.x - x + 0.5f);
+					var py = Mathf.FloorToInt(center.y + y + 0.5f);
+					var ny = Mathf.FloorToInt(center.y - y + 0.5f);
+ 
+					tex.SetPixel(px, py, Color.white);
+					tex.SetPixel(nx, py, Color.white);
+  
+					tex.SetPixel(px, ny, Color.white);
+					tex.SetPixel(nx, ny, Color.white);
+				}
+			}    
+			
+			tex.Apply ();
+            //rend.material.mainTexture = tex;
+            prop.SetTexture("_MainTex", tex);
+            rend.SetPropertyBlock(prop);
+			yield return 0;
+		}
+	}
+}
